5f1bf4589e696b3367aca092ae8d6d5a15d2cbc1
Kyrre Sjobak <k.n.sjobak@fys.uio.no>
2012-02-07 15:07:59 +0100

    More standard export (still bound to my setup)

--- src/acdOptiGui/infoFrames/DataExtractor.py
+++ src/acdOptiGui/infoFrames/DataExtractor.py
@@ -130,8 +130,8 @@ class DataExtractor(InfoFrameComponent):
         pass
     
     def event_button_export(self,widget,data=None):
-        self.dataExtractor.export("/home/kyrre/PhD/OptiData/cellData.csv", ["GEOM.idw", "GEOM.eow", "ANA.RFpost_local.maxFieldsOnSurface[0].surf[0].mode[0].Hmax_norm[0]"])
-        self.dataExtractor.export("/home/kyrre/PhD/OptiData/cellDataRaw.csv")
+        #self.dataExtractor.export("/home/kyrre/PhD/OptiData/cellData.csv", ["GEOM.idw", "GEOM.eow", "ANA.RFpost_local.maxFieldsOnSurface[0].surf[0].mode[0].Hmax_norm[0]"])
+        self.dataExtractor.export("/home/kyrre/PhD/OptiData/cellData.csv")
     
     def event_button_lockdownRun(self,widget,data=None):
         if self.dataExtractor.lockdown:

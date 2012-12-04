7058743a1d0b802c6b36d2ae4064f32876f8bb81
Kyrre Sjobak <k.n.sjobak@fys.uio.no>
2012-12-04 18:34:34 +0100

    Add 3D plotting capability to DataExtractorPlots

--- src/acdOpti/AcdOptiDataExtractorPlot.py
+++ src/acdOpti/AcdOptiDataExtractorPlot.py
@@ -120,3 +120,106 @@ class DataExtractorPlot2D(AcdOptiDataExtractorPlot):
     def updateSettingsDict(self):
         self.settingsDict["varX"] = self.varX
         self.settingsDict["varY"] = self.varY
+
+
+class DataExtractorPlot3D(AcdOptiDataExtractorPlot):
+    
+    plotType = "DataExtractorPlot3D"
+
+    varX = None
+    varY = None
+    varZ = None
+    
+    def __init__(self, dataExtractor, settingsDictOrInstName):
+        assert settingsDictOrInstName != None
+        settingsDict = None
+        print settingsDictOrInstName
+        if type(settingsDictOrInstName) == str:
+            settingsDict = DataDict()
+            settingsDict.pushBack("plotType", self.plotType)
+            settingsDict.pushBack("instName", settingsDictOrInstName)
+            settingsDict.pushBack("varX", "")
+            settingsDict.pushBack("varY", "")
+            settingsDict.pushBack("varZ", "")
+        else:
+            settingsDict = settingsDictOrInstName
+        print settingsDict
+        
+        super(DataExtractorPlot3D,self).__init__(dataExtractor,settingsDict)
+        self.varX = settingsDict["varX"]
+        self.varY = settingsDict["varY"]
+        self.varZ = settingsDict["varZ"]
+        
+    def doExport(self,fname):
+        (X, Y, Z) = self.getData()
+        raise NotImplementedError
+        
+    def getData(self):
+        assert self.dataExtractor.lockdown
+        assert self.varX in self.dataExtractor.keyNames
+        assert self.varY in self.dataExtractor.keyNames
+        assert self.varZ in self.dataExtractor.keyNames
+        
+        X = []
+        Y = []
+        Z = []
+        
+        for (row, rcount) in zip(self.dataExtractor.dataExtracted, xrange(len(self.dataExtractor.dataExtracted))):
+            try:
+                x = float(row[self.varX])
+                y = float(row[self.varY])
+                z = float(row[self.varZ])
+                
+                X.append(x)
+                Y.append(y)
+                Z.append(z)
+            except KeyError:
+                pass
+            except ValueError:
+                print "Warning in DataExtractorPlot2D::getData(): Could not convert value in row", rcount, "to float, skipping!"
+        
+        return (X,Y,Z)
+    
+    def deduplicate(self,X,Y,Z):
+        """
+        Deduplicate by finding all unique pairs (x,y), for each such pair set Z = average Z over all pair occurences
+        """
+        assert len(X) == len(Y) and len(Y) == len(Z)
+        
+        #Dedup'ed variables
+        x = []
+        y = []
+        z = []
+        n = []
+        
+        def comparePair(x1,y1, x2,y2):
+            return x1==x2 and y1==y2
+        
+        #Find all unique paris
+        for i in xrange(len(X)):
+            prevFound = False
+            for j in xrange(len(x)):
+                if comparePair(X[i],Y[i],x[j],y[j]):
+                    prevFound = True
+                    break
+            if not prevFound:
+                x.append(X[i])
+                y.append(Y[i])
+        
+        #Average over z 
+        for j in xrange(len(x)):
+            n.append(0)
+            z.append(0.0)
+            for i in xrange(len(X)):
+                if comparePair(X[i],Y[i],x[j],y[j]):
+                    n[-1] += 1
+                    z[-1] += Z[i]
+            z[-1] /= float(n[-1])
+        
+        return(x,y,z,n)
+                
+    
+    def updateSettingsDict(self):
+        self.settingsDict["varX"] = self.varX
+        self.settingsDict["varY"] = self.varY
+        self.settingsDict["varZ"] = self.varZ
--- src/acdOptiGui/infoFrames/DataExtractorPlots.py
+++ src/acdOptiGui/infoFrames/DataExtractorPlots.py
@@ -110,8 +110,10 @@ class DataExtractorPlots(InfoFrameComponent):
         #data = pointer to plot object to manipulate
         if data.plotType == "DataExtractorPlot2D":
             self.frameManager.push(DataExtractorPlots_Plot2D(self.frameManager, data))
-            pass
-        
+        elif data.plotType == "DataExtractorPlot3D":
+            self.frameManager.push(DataExtractorPlots_Plot3D(self.frameManager, data))
+        else:
+            print "Error in DataExtractorPlots::event_button_manipulate(): Unknown plot type '" + data.plotType + "'"
     def event_button_delete(self,widget,data):
         #data = element index in plots to delete
         del self.dataExtractor.plots[data]
@@ -121,7 +123,7 @@ class DataExtractorPlots(InfoFrameComponent):
     
     def event_button_add(self,widget,data):
         
-        (type, name, response) = self.getTypeAndNameDialog_currentBase(("DataExtractorPlot2D",), "New plotter")
+        (type, name, response) = self.getTypeAndNameDialog_currentBase(("DataExtractorPlot2D","DataExtractorPlot3D"), "New plotter")
         
         if response == gtk.RESPONSE_OK:
             if name == None:
@@ -225,6 +227,224 @@ class DataExtractorPlots_Plot2D(InfoFrameComponent):
         xRange = abs(max(X)-min(X))
         yRange = abs(max(Y)-min(Y))
         plt.axis([min(X)-0.1*xRange, max(X)+0.1*xRange, min(Y)-0.1*yRange, max(Y)+0.1*yRange])
+        
+        plt.show()
+        
+        gtk.main()
+    
+    def event_button_close(self,widget,data):
+        self.saveToPlot()
+        self.frameManager.pop()
+
+class DataExtractorPlots_Plot3D(InfoFrameComponent):
+    """
+    3D plot
+    """
+    
+    plotObject = None
+    
+    __varXentry = None
+    __varYentry = None
+    __varZentry = None
+    
+    __plotDelunayButton = None
+    __plotDelunayColorsButton = None
+    __plot3DPointCloudButton = None
+    
+    __closeButton = None
+    
+    
+    def __init__(self, frameManager, plotObject):
+        InfoFrameComponent.__init__(self, frameManager)
+        assert isinstance(plotObject, DataExtractorPlot3D) 
+        self.plotObject = plotObject
+        
+        self.baseWidget = gtk.VBox()
+        
+        self.baseWidget.pack_start(gtk.Label("DataExtractorPlot3D, instName = " + self.plotObject.instName), padding=5, expand=False)
+        
+        varXbox = gtk.HBox()
+        varXbox.pack_start(gtk.Label("X variable:"), padding=5,expand=False)
+        self.__varXentry = gtk.Entry()
+        self.__varXentry.set_text(self.plotObject.varX)
+        varXbox.pack_start(self.__varXentry, padding=5, expand=True)
+        self.baseWidget.pack_start(varXbox, padding=5, expand=False)
+        
+        varYbox = gtk.HBox()
+        varYbox.pack_start(gtk.Label("Y variable:"), padding=5,expand=False)
+        self.__varYentry = gtk.Entry()
+        self.__varYentry.set_text(self.plotObject.varY)
+        varYbox.pack_start(self.__varYentry, padding=5, expand=True)
+        self.baseWidget.pack_start(varYbox, padding=5, expand=False)
+        
+        varZbox = gtk.HBox()
+        varZbox.pack_start(gtk.Label("Z variable:"), padding=5,expand=False)
+        self.__varZentry = gtk.Entry()
+        self.__varZentry.set_text(self.plotObject.varZ)
+        varZbox.pack_start(self.__varZentry, padding=5, expand=True)
+        self.baseWidget.pack_start(varZbox, padding=5, expand=False)
+        
+        self.baseWidget.pack_start(gtk.HSeparator(), padding=10, expand=False)
+        
+        self.baseWidget.pack_start(gtk.Label("Available variables:\n (textbox just to enable copy/paste, nothing is saved)"), padding=5,expand=False)
+        varString = ""
+        for k in self.plotObject.dataExtractor.keyNames:
+            varString += k + " "
+        varString = varString[:-1]
+        varEntry = gtk.Entry()
+        varEntry.set_text(varString)
+        self.baseWidget.pack_start(varEntry, padding=5, expand=True)
+        
+        self.baseWidget.pack_start(gtk.HSeparator(), padding=10, expand=False)
+        
+        self.__plotDelunayButton = gtk.Button("Show Delunay triangulation of points")
+        if not self.plotObject.dataExtractor.lockdown:
+            self.__plotDelunayButton.set_sensitive(False)
+        self.__plotDelunayButton.connect("clicked", self.event_button_plotDelunay, None)
+        self.baseWidget.pack_start(self.__plotDelunayButton, padding=5, expand=False)
+
+        self.__plotDelunayColorsButton = gtk.Button("Show tripcolor plot")
+        if not self.plotObject.dataExtractor.lockdown:
+            self.__plotDelunayColorsButton.set_sensitive(False)
+        self.__plotDelunayColorsButton.connect("clicked", self.event_button_plotDelunayColors, None)
+        self.baseWidget.pack_start(self.__plotDelunayColorsButton, padding=5, expand=False)
+        
+        self.__plot3DPointCloudButton = gtk.Button("Show 3D point cloud")
+        if not self.plotObject.dataExtractor.lockdown:
+            self.__plot3DPointCloudButton.set_sensitive(False)
+        self.__plot3DPointCloudButton.connect("clicked", self.event_button_plotPointcloud, None)
+        self.baseWidget.pack_start(self.__plot3DPointCloudButton, padding=5, expand=False)
+        
+        self.baseWidget.pack_start(gtk.HSeparator(), padding=10, expand=False)
+        
+        self.__closeButton = gtk.Button("_Close plot view")
+        self.__closeButton.connect("clicked", self.event_button_close, None)
+        self.baseWidget.pack_start(self.__closeButton,padding = 5, expand=False)
+
+        self.baseWidget.show_all()
+    
+    def saveToPlot(self):
+        x = self.__varXentry.get_text().strip()
+        y = self.__varYentry.get_text().strip()
+        z = self.__varZentry.get_text().strip()
+        if " " in x or " " in y:
+            mDia = gtk.MessageDialog(self.getBaseWindow(),
+                                     gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
+                                     gtk.MESSAGE_ERROR, gtk.BUTTONS_OK,
+                                     "Space in X, Y, or Z variable, not saving to plot." )
+            mDia.run()
+            mDia.destroy()
+            return
+        self.plotObject.varX = x
+        self.plotObject.varY = y
+        self.plotObject.varZ = z
+        self.plotObject.updateSettingsDict()
+        self.plotObject.dataExtractor.write()
+    
+    def event_button_plotDelunay(self, widget, data):
+        self.saveToPlot()
+        try:
+            import matplotlib.pyplot as plt
+            import matplotlib.tri as tri
+        except ImportError:
+            print "Could not import matplotlib, aborting plot. You should still be able to doExport()!"
+            return
+        (X,Y,Z) = self.plotObject.getData()
+        print "X:", X
+        print "Y:", Y
+        print "Z:", Z
+        print "Deduplicating..."
+        (X,Y,Z,N) = self.plotObject.deduplicate(X,Y,Z)
+        print "X:", X
+        print "Y:", Y
+        print "Z:", Z
+        print "N:", N
+        if len(X) < 3:
+            mDia = gtk.MessageDialog(self.getBaseWindow(),
+                                     gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
+                                     gtk.MESSAGE_ERROR, gtk.BUTTONS_OK,
+                                     "Need at least three distinct points to make grid, only have: X=%s, Y=%s" %(str(X), str(Y)) )
+            mDia.run()
+            mDia.destroy()
+            return
+        triang = tri.Triangulation(X, Y)
+        plt.triplot(triang, 'bo-')
+        
+        plt.xlabel(self.plotObject.varX)
+        plt.ylabel(self.plotObject.varY)
+        
+        xRange = abs(max(X)-min(X))
+        yRange = abs(max(Y)-min(Y))
+        plt.axis([min(X)-0.1*xRange, max(X)+0.1*xRange, min(Y)-0.1*yRange, max(Y)+0.1*yRange])
+        
+        plt.show()
+        
+        gtk.main()
+    
+    def event_button_plotDelunayColors(self, widget, data):
+        self.saveToPlot()
+        try:
+            import matplotlib.pyplot as plt
+            import matplotlib.tri as tri
+        except ImportError:
+            print "Could not import matplotlib, aborting plot. You should still be able to doExport()!"
+            return
+        (X,Y,Z) = self.plotObject.getData()
+        print "X:", X
+        print "Y:", Y
+        print "Z:", Z
+        print "Deduplicating..."
+        (X,Y,Z,N) = self.plotObject.deduplicate(X,Y,Z)
+        print "X:", X
+        print "Y:", Y
+        print "Z:", Z
+        print "N:", N
+        if len(X) < 3:
+            mDia = gtk.MessageDialog(self.getBaseWindow(),
+                                     gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
+                                     gtk.MESSAGE_ERROR, gtk.BUTTONS_OK,
+                                     "Need at least three distinct points to make grid, only have: X=%s, Y=%s" %(str(X), str(Y)) )
+            mDia.run()
+            mDia.destroy()
+            return
+        triang = tri.Triangulation(X, Y)
+        plt.tripcolor(triang, Z, shading='gouraud')
+        plt.colorbar()
+
+        plt.xlabel(self.plotObject.varX)
+        plt.ylabel(self.plotObject.varY)
+        
+        xRange = abs(max(X)-min(X))
+        yRange = abs(max(Y)-min(Y))
+        plt.axis([min(X)-0.1*xRange, max(X)+0.1*xRange, min(Y)-0.1*yRange, max(Y)+0.1*yRange])
+        
+        plt.show()
+        
+        gtk.main()
+    
+    def event_button_plotPointcloud(self, widget, data):
+        self.saveToPlot()
+        try:
+            import matplotlib.pyplot as plt
+            from mpl_toolkits.mplot3d import Axes3D
+        except ImportError:
+            print "Could not import matplotlib, aborting plot. You should still be able to doExport()!"
+            return
+        (X,Y,Z) = self.plotObject.getData()
+        print "X:", X
+        print "Y:", Y
+        print "Z:", Z
+        
+        fig = plt.figure()
+        ax = fig.gca(projection='3d')
+        ax.scatter(X, Y, Z)
+        
+        #xRange = abs(max(X)-min(X))
+        #yRange = abs(max(Y)-min(Y))
+        
+        ax.set_xlabel(self.plotObject.varX)
+        ax.set_ylabel(self.plotObject.varY)
+        ax.set_zlabel(self.plotObject.varZ)
         
         plt.show()
         
